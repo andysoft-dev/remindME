50b4a0f576c5eadbb6c2d0a53a88bf151c6919c3
Andy Jara <andy@desarrollador.cl>
2022-09-18 19:26:33 -0300

    Add installer Linux

---
+++ remindME/instalador.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+
+echo "Instalador de remindME"
+
+cd /etc/profile.d/
+
+printf "#!/bin/bash\nexport PATH=\$PATH:/usr/local/bin/remindME" > reminder.sh
+
+tar -xf remindME-Linux.tar.gz -C /user/local/bin/remindME/
+cd /user/local/bin/remindME
+chmod -x remindME
+
+
